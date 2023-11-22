934e03a18754f58b4473dea741cdaebe4b2e7943
Eddie Kao <eddie@5xcampus.com>
2023-11-22 13:31:44 +0800

    install tailwind & daisyui

--- src/App.vue
+++ src/App.vue
@@ -2,56 +2,14 @@
 export default {
   data() {
     return {
-      title: "AUO Project",
-      devices: ["A", "B", "C", "D", "E"],
-      show: true,
-      website: "https://www.auo.com/zh-TW",
-      num_x: 1,
-      num_y: 2,
-      // result: 0,
+      title: "AUO 圖書管理系統",
     }
-  },
-  watch: {
-    num_x(newValue, oldValue) {
-      console.log({ newValue, oldValue })
-    },
-  },
-  methods: {
-    printout() {
-      console.log(this.title)
-    },
-    toggleHello() {
-      this.show = !this.show
-    },
-    // calc() {
-    //   this.result = this.num_x + this.num_y
-    // },
-  },
-  computed: {
-    result() {
-      return this.num_x + this.num_y
-    },
   },
 }
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
-  <input type="text" v-model="title" />
-
-  <br />
-  <input type="number" v-model="num_x" />
-  <input type="number" v-model="num_y" />
-  <div>結果 {{ result }}</div>
-
-  <ul>
-    <li v-for="d in devices">{{ d }}</li>
-  </ul>
-  <a :href="website">AUO Website</a>
-  <button @click="printout">click</button>
-  <hr />
-  <button @click="toggleHello">Toggle</button>
-  <h2 v-if="show">Hello</h2>
+  <h1 class="text-slate-800 text-5xl">{{ title }}</h1>
 </template>
 
 <style scoped></style>
