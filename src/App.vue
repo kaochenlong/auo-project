ced03b5aaef9054cb717636e2aedbc9e310154bf
Eddie Kao <eddie@5xcampus.com>
2023-11-29 14:16:59 +0800

    rewrite App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,45 @@
-<script>
+<script setup>
 import { v4 as uuid } from "uuid"
 import { getBooks, saveBooks } from "@/auo-lib/storage"
+import { reactive, onBeforeMount } from "vue"
 import Header from "./components/infomation/Header.vue"
 import BookItem from "./components/books/Item.vue"
 import AddBookForm from "./components/forms/AddBook.vue"
 
-export default {
-  data() {
-    return {
-      title: "AUO 圖書管理系統",
-      books: [],
+const title = "AUO 圖書管理系統"
+const books = reactive([])
+
+const removeBook = (id) => {
+  if (id) {
+    const bookIndex = books.findIndex((book) => book.id == id)
+
+    if (bookIndex >= 0) {
+      books.splice(bookIndex, 1)
+      saveBooks(books)
     }
-  },
-  components: { Header, BookItem, AddBookForm },
-  beforeMount: function () {
-    const books = getBooks()
-    if (books) {
-      this.books = books
-    }
-  },
-
-  methods: {
-    removeBook(id) {
-      if (id) {
-        const bookIndex = this.books.findIndex((book) => book.id == id)
-
-        if (bookIndex >= 0) {
-          this.books.splice(bookIndex, 1)
-          saveBooks(this.books)
-        }
-      }
-    },
-    addBook(bookName) {
-      if (bookName != "") {
-        const book = {
-          id: uuid(),
-          title: bookName,
-        }
-        this.books.unshift(book)
-
-        // save
-        saveBooks(this.books)
-      }
-    },
-  },
+  }
 }
+
+const addBook = (bookName) => {
+  if (bookName != "") {
+    const book = {
+      id: uuid(),
+      title: bookName,
+    }
+
+    books.unshift(book)
+
+    // save
+    saveBooks(books)
+  }
+}
+
+onBeforeMount(() => {
+  const data = getBooks()
+  if (data) {
+    books.push(...data)
+  }
+})
 </script>
 
 <template>
